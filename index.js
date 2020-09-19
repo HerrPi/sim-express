9b0b15e64ca3ab829fd9631dc178bb950749dc9c
Simon Brenner <herrpi@gmail.com>
2020-09-20 00:43:24 +0200

    CORS added

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ let db;
 //db.close()
 
 // Express stuff
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })

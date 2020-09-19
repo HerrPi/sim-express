232063143611965bce0fbe4118413c8dd65bcd4f
Simon Brenner <herrpi@gmail.com>
2020-09-20 00:05:52 +0200

    Some routing. SQLite tests. Took own db into project, but gitignored.

--- index.js
+++ index.js
@@ -1,9 +1,52 @@
+// start with node index.js
+
 const express = require('express')
+const sqlite3 = require('sqlite3')
 const app = express()
 const port = 3000
 
+// sqlite stuff
+let db;
+//db.close()
+
+// Express stuff
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+app.get('/config/:configparam', (req, res) => {
+    if (req.params.configparam === "dbconnect") {
+        db = new sqlite3.Database('./gdve.sqlite3', (err) => {
+            if (err) {
+                console.error(err.message);
+            }
+            res.send('DB GdvE connected.')
+        });
+    } else if (req.params.configparam === "dbdisconnect") {
+        db.close()
+        res.send('DB closed.')
+    } else {
+        res.send('Unknown command.')
+    }
+})
+app.get('/downloads/:dlid', (req, res) => {
+    db.all(`SELECT * FROM downloads`, [], (err, rows) => {
+        if (err) {
+            throw err
+        }
+        res.json(rows.find(x => x.id === req.params.dlid).filename)
+    })
+})
+
+app.post('/', (req, res) => {
+    res.send('Ok, das war ein POST!')
+})
+
+app.put('/', (req, res) => {
+    res.send('Ok, das war ein PUT!')
+})
+
+app.delete('/', (req, res) => {
+    res.send('Ok, das war ein DELETE!')
 })
 
 app.listen(port, () => {
